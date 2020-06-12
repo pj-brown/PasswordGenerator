ead0224b26331412cc8d7f25f5bb155e9145769b
Patrick Brown <pjbrown05@gmail.com>
2020-06-12 16:45:07 -0500

    initial pseudocoding

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Need variables for lowercase, uppercase, numbers, and special characters
+var lowerCase = []
+var upperCase = []
+var numbers = []
+var specialChars = []
+var passwordLength = []
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +18,19 @@ function writePassword() {
   passwordText.value = password;
 
 }
+// Generate 5 prompts for user - password length, lowercase, uppercase, numeric, and/or special char
+passwordLength prompt("Please choose password length between 8 and 128 characters.")
+prompt("Include lowercase letters?")
+prompt("Include uppercase letters?")
+prompt("Include numbers?")
+prompt("Include special characters?")
+
+// Need if/else statements for user prompt selections 
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
