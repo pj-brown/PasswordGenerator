4cf7ec545d5c893fa4733545ae4c11bfc5f8db09
Patrick Brown <pjbrown05@gmail.com>
2020-06-12 17:23:37 -0500

    added variables and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -17,18 +17,35 @@
   // written to the page
 
 // VARIABLES
-  // Declare variables for storing the following:
-    // a) - password length
-
-    // b) - lowercase
-    // c) - uppercase
-    // d) - numbers
-    // e) - special characters
-
-// Assignment Code
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = Math.floor(Math.random()*129);
+var specialChars = "!@#$%^&*()_";
+var passwordLength = Math.floor(Math.random()*129);
+// PROMPTS
+// these prompts need to be rendered when the user selects the "Generate Password" button (so they would be part of a the function of generatePassword)
+let isLength = prompt("Please choose password length between 8 and 128 characters.", "A number between 8 and 128 is required");
+alert("Warning! A minimum of one of the following four prompts MUST be selected in order to generate a password");
+let isLowercase = confirm("Include lowercase letters?");
+let isUppercase = confirm("Include uppercase letters?");
+let isNumbers = confirm("Include numbers?");
+let isSpecialChars = confirm("Include special characters?");
+// Assignment Code (I am assuming assignemnt code would cover both prompts & variables?)
 var generateBtn = document.querySelector("#generate");
 
+// Now I need to figure out how to take the answers given by the user to change the outcome of the password to be generated...
+
+
 // FUNCTIONS
+// need a function to loop back through the prompts if invalid password length is selected
+for (let i = 0; i < array.length; i++) {
+  const isLength != passwordLength;
+  else 
+  
+}
+// need a function to loop back through the prompts if only password length is selected (or do i just need for loops?)
+
+
 // Write password to the #password input (writePassword function responsible for choosing a random password and rendering it to the page [as a string? or is a variable determined by the function?])
 function writePassword() {
   var password = generatePassword();
